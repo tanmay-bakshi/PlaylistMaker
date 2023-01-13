30415c86cfc3c62e6ac8e4a8d70bdabcc6b7c884
Michael Francis <IanMichaelFrancis@outlook.com>
2023-01-12 23:50:57 -0500

    update formatting

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import os
 import requests
 import sys
 
+from pathlib import Path
 from flask import Flask, request
+from typing import Any, Dict, List, Optional, Tuple
+
 app = Flask(__name__)
 
-
-with open("credentials", "r", encoding="utf-8") as file:
+with (Path(__file__).parent / "credentials").open("r", encoding="utf-8") as file:
     contents = file.readlines()
     if len(contents) != 2:
         print("Error: Credentials file must only contain 2 credentials!", file=sys.stderr)
@@ -18,100 +20,25 @@ with open("credentials", "r", encoding="utf-8") as file:
     CLIENT_SECRET = contents[1].strip()
 
 ENCODED_CLIENT = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode("ascii")).decode("ascii")
-DENYLIST = {
+SWAPLIST = {
     "they're": "they are",
     "year's": "years",
 }
 
-def get_page(auth_token, subphrase, track_limit, offset):
-    subphrase = subphrase.replace(" ", "%20")
-    url = f"https://api.spotify.com/v1/search?q={subphrase}&type=track&limit={track_limit}&offset={offset}"
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {auth_token}",
-    }
-    response = requests.get(url=url, headers=headers).json()
-    if "tracks" in response and "items" in response["tracks"]:
-        return response["tracks"]["items"]
-    else:
-        return {}
+AUTH_TOKEN: Optional[str] = None
 
-def search_songs(auth_token, subphrase, track_limit):
-    offset = 0
-    page = 0
-    while True:
-        page_of_results = get_page(auth_token, subphrase, track_limit, offset)
-        if len(page_of_results) == 0:
-            print(f"\nCannot find: '{subphrase}'")
-            return None
-        else:
-            page += 1
-            for song in page_of_results:
-                print(f"\rSearching page {page} for '{subphrase}'", end="")
-                if song["name"].lower() == subphrase:
-                    print(f'\nFOUND: {song["name"]} by {song["artists"][0]["name"]}')
-                    return song
-            offset += track_limit
 
-def get_songs(auth_token, phrase, track_limit):
-    subphrase = " ".join(phrase)
-    songs_to_add = []
-    cache = {}
-    while len(phrase) > 0:
-        for i in range(0 if len(phrase) <= 5 else len(phrase) - 4, len(phrase)):
-            subphrase = " ".join(phrase[:-i] if i != 0 else phrase)
-            if subphrase in DENYLIST:
-                subphrase = blacklist[subphrase]
-            if subphrase in cache:
-                song = cache[subphrase]
-                print(f"Using cached result for '{subphrase}'")
-            else:
-                song = search_songs(auth_token, subphrase, track_limit)
-            if song != None:
-                phrase = [] if i == 0 else phrase[-i:]
-                break
-        if song is not None:
-            songs_to_add.append(song)
-            cache[subphrase] = song
-        else:
-            break
+def main() -> None:
+    """
+    Main execution of the program.
+    """
 
-    return songs_to_add
 
-def create_playlist(auth_token, playlist_name, userid):
-    print("Creating playlist...")
-    url = f"https://api.spotify.com/v1/users/{userid}/playlists"
-    data = json.dumps({
-      "name": f"{playlist_name}",
-      "description": f"{' '.join(phrase)}",
-      "public": True,
-    })
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {auth_token}",
-    }
-
-    response = requests.post(url=url, data=data, headers=headers).json()
-    playlist_id = response["id"]
-    playlist_url = response["external_urls"]["spotify"]
-
-    return playlist_id, playlist_url
-
-def add_to_playlist(auth_token, songs, playlist_id):
-    print("Adding songs to playlist...")
-    uris = ",".join([song["uri"]for song in songs])
-    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?uris={uris}"
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {auth_token}",
-    }
-    response = requests.post(url=url, headers=headers)
-
-@app.route('/auth_callback')
-def callback():
+@app.route("/auth_callback")
+def callback() -> str:
+    """
+    This function connects with the spotify app.
+    """
     url = "https://accounts.spotify.com/api/token"
     data = {
         "code": request.args["code"],
@@ -122,38 +49,203 @@ def callback():
         "Authorization": f"Basic {ENCODED_CLIENT}",
         "Content-Type": "application/x-www-form-urlencoded",
     }
-    response = requests.post(url=url, data=data, headers=headers).json()
-    auth_token = response["access_token"]
+    response = requests.post(url=url, data=data, headers=headers, timeout=600).json()
 
-    track_limit = 50 # max value
+    global AUTH_TOKEN  # pylint: disable=global-statement
+    AUTH_TOKEN = response["access_token"]
+
     userid = "s11vr90hshxqiw5juux7xiw0n"
 
-    songs = get_songs(auth_token, phrase, track_limit)
+    songs = get_songs(sanitized_phrase)
     print("All songs found!")
-    playlist_id, playlist_url = create_playlist(auth_token, playlist_name, userid)
-    add_to_playlist(auth_token, songs, playlist_id)
+    playlist_id, playlist_url = create_playlist(user_playlist_name, sanitized_phrase, userid)
+    add_to_playlist(songs, playlist_id)
 
     print(playlist_url)
     os.system(f"open {playlist_url}")
     return ""
 
+
+def get_page(subphrase: str, track_limit: int, offset: int) -> Optional[List[Dict[str, Any]]]:
+    """
+    Get the page of songs that contain the subphrase in their titles.
+
+    Args:
+        subphrase:   The subphrase to be matched within a song title.
+
+        track_limit: The number of songs in the page.
+
+        offset:      The page number.
+
+    Returns:
+        The dictionary of matching songs, if any.
+    """
+    subphrase = subphrase.replace(" ", "%20")
+    url = f"https://api.spotify.com/v1/search?q={subphrase}&type=track&limit={track_limit}&offset={offset}"
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {AUTH_TOKEN}",
+    }
+    response = requests.get(url=url, headers=headers, timeout=600).json()
+    if "tracks" in response and "items" in response["tracks"]:
+        return response["tracks"]["items"]
+
+    return None
+
+
+def search_songs(subphrase: str) -> Optional[Dict[str, Any]]:
+    """
+    Find the song, if any, with a name that is an exact match for the provided string.
+
+    Args:
+        subphrase:  The subphrase to be matched by a song title.
+
+    Returns:
+        The song whose title is an exact match for the subphrase (if any).
+    """
+    offset = 0
+    page = 0
+    track_limit = 50  # max value
+    while True:
+        page_of_results = get_page(subphrase, track_limit, offset)
+        if page_of_results is None:
+            print(f"\nCannot find: '{subphrase}'")
+            return None
+
+        page += 1
+        print(f"\rSearching page {page} for '{subphrase}'", end="")
+        for song in page_of_results:
+            if song["name"].lower() == subphrase:
+                print(f'\nFOUND: {song["name"]} by {song["artists"][0]["name"]}')
+                return song
+        offset += track_limit
+
+
+def get_songs(phrase: List[str]) -> List[Dict[str, str]]:
+    """
+    Get the list of songs that, together, make up the given phrase.
+
+    Args:
+        phrase: The phrase to be converted into a list of songs.
+
+    Returns:
+        The list of songs to match the phrase, with each song represented as a dictionary.
+        This dictionary includes information such as track name and artist.
+    """
+    subphrase = " ".join(phrase)
+    songs_to_add: List[Dict[str, Any]] = []
+    cache: Dict[str, Dict[str, Any]] = {}
+    while len(phrase) > 0:
+        song: Optional[Dict[str, Any]] = None
+        for i in range(0 if len(phrase) <= 5 else len(phrase) - 4, len(phrase)):
+            subphrase = " ".join(phrase[:-i] if i != 0 else phrase)
+            subphrase = SWAPLIST.get(subphrase, subphrase)
+            if subphrase in cache:
+                song = cache[subphrase]
+                print(f"Using cached result for '{subphrase}'")
+            else:
+                song = search_songs(subphrase)
+            if song is not None:
+                phrase = [] if i == 0 else phrase[-i:]
+                break
+        if song is not None:
+            songs_to_add.append(song)
+            cache[subphrase] = song
+        else:
+            break
+
+    return songs_to_add
+
+
+def create_playlist(playlist_name: str, description: List[str], userid: str) -> Tuple[str, str]:
+    """
+    Create an empty Spotify playlist.
+
+    Args:
+        playlist_name: The playlist title.
+
+        description:   The description for the playlist.
+
+        userid:        The user ID for the account that will own the playlist.
+
+    Returns:
+        The unique playlist ID and the playlist's URL.
+    """
+    print("Creating playlist...")
+    url = f"https://api.spotify.com/v1/users/{userid}/playlists"
+    data = json.dumps(
+        {
+            "name": f"{playlist_name}",
+            "description": f"{' '.join(description)}",
+            "public": True,
+        }
+    )
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {AUTH_TOKEN}",
+    }
+
+    response = requests.post(url=url, data=data, headers=headers, timeout=600).json()
+    playlist_id = response["id"]
+    playlist_url = response["external_urls"]["spotify"]
+
+    return playlist_id, playlist_url
+
+
+def add_to_playlist(songs: List[Dict[str, str]], playlist_id: str) -> None:
+    """
+    Append a pre-existing Spotify playlist.
+
+    Args:
+        songs:       The list of songs to add to the playlist.
+                     Each song must be represented as a dictionary containing
+                     the song's URI, in the <key>: <value> form of:
+                     "uri": <value>.
+
+        playlist_id: The unique ID of the playlist to append.
+
+    Raises:
+        ValueError: If Spotify returns an error when attempting to update
+                    the playlist.
+    """
+    print("Adding songs to playlist...")
+    uris = ",".join([song["uri"] for song in songs])
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?uris={uris}"
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {AUTH_TOKEN}",
+    }
+    response = requests.post(url=url, headers=headers, timeout=600)
+    if "error" in response.json():
+        raise ValueError
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("""
+        print(
+            """
 Usage:
     python3 main.py "phrase" "playlist name"
-""")
-    else:
-        phrase = sys.argv[1].lower()
-        # Filter out non-alphabetical characters
-        phrase = "".join([x for x in phrase if (ord(x) >= ord('a') and ord(x) <= ord('z')) or x == " " or x == "'"]).split()
-        playlist_name = sys.argv[2]
-
-        os.system("open https://accounts.spotify.com"
-                  f"/authorize?response_type=code\\&client_id={CLIENT_ID}"
-                  "\\&scope=playlist-modify-public"
-                  "\\&redirect_uri=http://localhost:8509/auth_callback"
-                  "\\&state=ramranch"
+        """
         )
+    #        return 1
 
-        app.run(port=8509, host="0.0.0.0")
+    user_phrase = sys.argv[1].lower()  # pylint: disable=invalid-name
+    # Filter out non-alphabetical characters
+    sanitized_phrase = "".join(
+        [x for x in user_phrase if (ord(x) >= ord("a") and ord(x) <= ord("z")) or x == " " or x == "'"]
+    ).split()
+    user_playlist_name = sys.argv[2]  # pylint: disable=invalid-name
+
+    os.system(
+        "open https://accounts.spotify.com"
+        f"/authorize?response_type=code\\&client_id={CLIENT_ID}"
+        "\\&scope=playlist-modify-public"
+        "\\&redirect_uri=http://localhost:8509/auth_callback"
+        "\\&state=ramranch"
+    )
+
+    app.run(port=8509, host="0.0.0.0")
